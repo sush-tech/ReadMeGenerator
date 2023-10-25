dcfbc5e3b78c6811de6278a895de154aef89b4b2
sush-tech <sushmithar31@gmail.com>
2023-10-24 21:37:39 -0400

    final read me generator

---
+++ dist/README.md
@@ -0,0 +1,39 @@
+# ss
+
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  ss
+
+  ## Installation
+  ss
+
+  ## Usage
+  ss
+
+  ## Licenses
+    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
+
+  ## Contributing
+  ss
+
+  ## Tests
+  ss
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/sss  
+  Email: ss
+
+  ## Credits
+  sush
--- index.js
+++ index.js
@@ -1,36 +1,181 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+//imports needed 
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = ["what si yoru readme about?", "what si this?", "what si that"];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.log(err) : console.log('Success!')
-    );
-}
-
-// TODO: Create a function to initialize app
-function init() {
-
-for(let i=0;i<questions.length;i++){
-    return inquirer
-    .prompt([
-      {
+// Array of questions
+const questions = [
+    {
         type: 'input',
-        name: 'questions',
-        message: questions[i],
-      },  
-    ])
-    .then((data) => {
-      const filename = `readMe.md`;
-      writeToFile(filename,data);
-    });
+        name: 'name',
+        message: 'Welcome! Whats your full name:',
+        validate: userName => {
+            if (userName) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+        validate: githubUserName => {
+            if (githubUserName) {
+                return true;
+            } else {
+                console.log('please enter your github link');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: emailId => {
+            if (emailId) {
+                return true;
+            } else {
+                console.log('Please enter your email');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Whats the title of your project?',
+        validate: title => {
+            if (title) {
+                return true;
+            } else {
+                console.log('Please enter the title of your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter your project description here:",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('enter the description of your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation?',
+        validate: installation => {
+            if (installation) {
+                return true;
+            } else {
+                console.log('Please provide instructions for installation');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instructions for usage:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Providing instructions for usage');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        validate: contribution => {
+            if (contribution) {
+                return true;
+            } else {
+                console.log('Please provide instructions on how others can contribute to your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe the tests written for your application and how to use them:',
+        validate: tests => {
+            if (tests) {
+                return true;
+            } else {
+                console.log('Describe the tests written for your application and how to use them');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicenses',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'What license would you like to include?',
+        choices: ['MIT', 'GPL', 'CC--0'],
+        when: ({ confirmLicenses }) => {
+            if (confirmLicenses) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
+
+// Function to write README file
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        // make a readme file and add to dist folder
+        fs.writeFile('./dist/README.md', data, err => {
+            // if there's an error, reject the Promise and send the error to .catch() method
+            if (err) {
+                reject (err);
+                // return out of the function here to make sure the Promise doesn't continut to execute the resolve() function
+                return;
+            }
+            // if everything went well, resolve the Promise and send the successful data to the .then() method
+            resolve({
+                ok: true,
+                message: console.log('Success! Navigate to the "dist" folder to see your README!')
+            });
+        })
+    })
 }
+
+// Initialize app
+const init = () => {
+    return inquirer.prompt(questions);
 }
 
 // Function call to initialize app
-init();
-
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
